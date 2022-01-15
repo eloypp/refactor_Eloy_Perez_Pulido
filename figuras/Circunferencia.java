75530ec82d23f672cfc38b0dec64b69339d13ac8
Eloy <Eloypp55@gmail.com>
2022-01-15 18:35:14 +0000

    Este es el séptimo commit hecho por Eloy Pérez Pulido

--- figuras/Circunferencia.java
+++ figuras/Circunferencia.java
@@ -10,7 +10,8 @@ public class Circunferencia {
 
     public void imprimir() {
         color = "rojo";
-        System.out.println("Di�metro: " + 2 * radio);
+        double d = 2 * radio;
+        System.out.println("Di�metro: " + d);
         System.out.println("Color: " + color);
         double area = 2 * 3.1416 * radio * radio;
         System.out.println(area);
